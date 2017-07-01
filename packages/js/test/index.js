7dbacdf6b1e1bcd7b66821afe1bc61b980d2ef52
Dominic Barnes <dominic@dbarnes.info>
2017-07-01 01:45:04 -0700

    tests(js): raise sshpk test timeout again (60s)

--- packages/js/test/index.js
+++ packages/js/test/index.js
@@ -229,7 +229,7 @@ describe('js plugin', function () {
     })
 
     it('should work with sshpk', function () {
-      this.timeout('30s')
+      this.timeout('60s')
 
       let entry = fixture('circular-deps-4/index.js')
       return mako({ root: fixture('circular-deps-4') })
